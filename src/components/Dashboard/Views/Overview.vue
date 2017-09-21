58f9920c9ac7028c222c5db99afac3f851d8c0c8
Damian Lion Maring <damianmaring@googlemail.com>
2017-09-21 23:44:04 +0200

    add licence

--- src/components/Dashboard/Views/Overview.vue
+++ src/components/Dashboard/Views/Overview.vue
@@ -23,7 +23,7 @@
     <div class="row">
 
       <div class="col-xs-12">
-        <live-card></live-card>
+        <!-- here should be a plotter of the water level -->
       </div>
 
       <div class="col-md-6 col-xs-12">
